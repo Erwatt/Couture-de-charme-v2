9fd24e2f5dfda6bea7c663d345fde0069c534994
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-07-01 18:06:26 +0200

    Footer

--- la-couture-du-charme/client/src/Components/App.js
+++ la-couture-du-charme/client/src/Components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
 import '../CSS/App.css';
 
 
@@ -8,11 +9,14 @@ import '../CSS/App.css';
 function App() {
   return (
     <div className="App">
-      <div id="my-header">
+      <div>
         <Header/>
       </div>
       <div className="content">
         <Home/>
+      </div>
+      <div>
+        <Footer/>
       </div>
     </div>
   );
---
+++ la-couture-du-charme/client/src/Components/Footer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from '../Images/logo.png';
+import '../CSS/Footer.css';
+
+function Footer(){
+    return (
+        <div className="footer">
+            <div className="footer-up">
+                <div className="footer-contact-map">
+                    <div className="footer-contact">
+                        <h2>COUTURE DU CHARME</h2>
+                        <p>1400, rue de Beaumetz</p>
+                        <p>59310 SAMÉON</p>
+                        <h3>📞 03 27 41 52 47</h3>
+                    </div>
+                    <img src={logo} alt="logo" className="footer-logo"/>
+                </div>
+                <form className="footer-message">
+                    <div className="footer-message-form">
+                        <div className="footer-message-form-inputs">
+                            <div className="footer-message-form-input">
+                                <label htmlFor="Name">Nom et prénom</label>
+                                <input name="Name"/>
+                            </div>
+                            <div className="footer-message-form-input">
+                                <label htmlFor="Phone">Numéro de téléphone</label>
+                                <input name="Phone"/>
+                            </div>
+                            <div className="footer-message-form-input">
+                                <label htmlFor="Mail">E-mail</label>
+                                <input name="Mail"/>
+                            </div>
+                            <div className="footer-message-form-input">
+                                <label htmlFor="Spam">Anti-spam</label>
+                                <input name="Spam"/>
+                            </div>
+                        </div>
+                        <div className="footer-message-form-txtarea">
+                            <label htmlFor="Message">Message</label>
+                            <textarea name="Message"/>
+                        </div>
+                    </div>
+                    <button className="footer-message-form-send">📩 Envoyer le message</button>
+                </form>
+            </div>
+            <div className="footer-down">
+                <p>Couture du Charme 2021</p>
+                <p className="footer-down-mention">📜 Mentions légales</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
