88f291898ef434a7619808c94c21116290437ef2
mege <mathias.mege@isen.yncrea.fr>
2021-07-05 10:15:11 +0200

    ajout PrivateSpa.js + css

--- la-couture-du-charme/client/src/Components/HomeSpa.js
+++ la-couture-du-charme/client/src/Components/HomeSpa.js
@@ -1,19 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import '../CSS/HomeSpa.css';
 
 function HomeSpa() {
   return (
     <div className="container-Spa">
       <div className="borderSpa">
-        <div className="text-containerSpa">
-             <div className = "titleSpa">
+        <Link to="/PrivateSpa" className='Spa-link'><div className="text-containerSpa">
+             <h2 className = "titleSpa">
                  Spa privatif
-             </div>
-             <div className = "textSpa">
+             </h2>
+             <p className = "textSpa">
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus id interdum velit laoreet id donec. Euismod nisi porta lorem mollis aliquam ut porttitor leo. 
-             </div>
+             </p>
          </div>
+         </Link>
          </div>
          <div className="imageSpa"></div>
 
--- la-couture-du-charme/client/src/Components/PrivateSpa.js
+++ la-couture-du-charme/client/src/Components/PrivateSpa.js
@@ -4,22 +4,22 @@ import chambre from '../Images/chambre.jpeg';
 import spa from '../Images/spa.jpeg';
 import logo from '../Images/logo.png';
 import piscine from '../Images/piscine.jpeg';
-import '../CSS/PrivateRoom.css';
+import '../CSS/PrivateSpa.css';
 import Promise from './Promise';
 
-export default function PrivateRoom(){
+export default function PrivateSpa(){
     const picturesList = [chambre, spa, logo, piscine]
 
     return(
         <div>
-                <div className='room'>
+                <div className='spa'>
                 <Promise/>
-                <div className='room-diapo'><Diaporama picturesList ={picturesList}/></div>
-                <div className='room-info'><h1>Lorem ispum dolor</h1>
-                    <div className='room-info-txt'>
+                <div className='spa-diapo'><Diaporama picturesList ={picturesList}/></div>
+                <div className='spa-info'><h1>Lorem ispum dolor</h1>
+                    <div className='spa-info-txt'>
                         <p>blablablabla</p>
                         <p>blublublublu</p>
-                        </div>
+                    </div>
                 </div> 
             </div>
         </div>
