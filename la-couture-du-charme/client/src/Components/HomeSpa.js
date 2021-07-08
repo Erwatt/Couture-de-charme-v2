7d9fbe2dc86bdcbed72cc674e9029f33890255cd
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-07-08 15:41:54 +0200

    test

--- la-couture-du-charme/client/src/Components/HomeSpa.js
+++ la-couture-du-charme/client/src/Components/HomeSpa.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../CSS/HomeSpa.css';
+import imgSpa from '../Images/spa.jpeg'
 
 function HomeSpa() {
   return (
     <div className="container-Spa">
-      <div className="borderSpa">
-        <Link to="/PrivateSpa" className='Spa-link'><div className="text-containerSpa">
-             <h2 className = "titleSpa">
-                 Spa privatif
-             </h2>
-             <p className = "textSpa">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus id interdum velit laoreet id donec. Euismod nisi porta lorem mollis aliquam ut porttitor leo. 
-             </p>
-         </div>
-         </Link>
-         </div>
-         <div className="imageSpa"></div>
-
-          
+      <img src={imgSpa} alt="Canvas Logo" className="imageSpa"/>
+      <div className="containerTextBorder">
+        <div className="containerText">
+          <h3>Spa privatif</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida rutrum quisque. Purus non enim praesent elementum facilisis leo vel. Duis convallis convallis tellus id interdum velit laoreet id. Neque sodales ut etiam sit amet nisl purus. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis.</p>
+        </div>
       </div>
+    </div>
   );
 }
 
