13f297aae8779cb99fe17330b543f853b03369ab
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-07-16 10:44:39 +0200

    promise3

--- la-couture-du-charme/client/src/Components/Promise2.js
+++ la-couture-du-charme/client/src/Components/Promise2.js
@@ -6,16 +6,28 @@ export default function Promise2(){
     return(
         <div className="containerPromise2">
             <div className="containerFond">
-            <p className='citation'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <div className="groupelement">
-                <div className="element" id="chambres">Chambres</div>
-                <div className="element" id="piscine">Piscine</div>
-                <div className="element" id="spa">Spa</div>
-                <div className="element" id="modelages">Modelages</div>
+                <div className='citation'>
+                    <p>UNE PARENTHÈSE DANS VOTRE QUOTIDIEN</p>
+                    {/* <p>DANS VOTRE QUOTIDIEN</p> */}
+                </div>
+                <div className="groupelement">
+                    <div className="element" id="chambres">Chambres</div>
+                    <div className="element" id="piscine">Piscine</div>
+                    <div className="element" id="spa">Spa</div>
+                    <div className="element" id="modelages">Modelages</div>
+                </div>
             </div>
-            </div>
-
-            
+            <div className="promise-quiSommesNous">
+                    <p>Au coeur d'une nature calme et verdoyante, la Couture du Charme est le lieu
+                                                    unique pour vous offrir un moment de détente inoubliable, entre amis ou en famille.
+                    </p>
+                    <p>Relaxez-vous dans notre spa privatif avec piscine, séjournez dans nos chambre d'hôtes
+                                                    spacieuses et modernes, détendez-vous grâce aux bienfaits de nos modelages...
+                    </p>
+                    <p>La Couture du Charme vous accueille à Saméon, à proximité d'Orchies et Saint Amand, 
+                                                    à 20 minutes de Lille et Valenciennes.
+                    </p>
+            </div>            
         </div>
     )
 
