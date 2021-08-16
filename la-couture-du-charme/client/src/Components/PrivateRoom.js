d88984dd3ea1a3a78817525d5b6fac385b1ef0de
mege <mathias.mege@isen.yncrea.fr>
2021-08-16 11:08:25 +0200

    modifs privateRoom

--- la-couture-du-charme/client/src/Components/PrivateRoom.js
+++ la-couture-du-charme/client/src/Components/PrivateRoom.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Diaporama from './Diaporama';
-import '../CSS/PrivateRoom.css';
+import '../CSS/PrivateRoom.scss';
 import cocoon1 from '../Images/cocoon1.jpg';
 import cocoon2 from '../Images/cocoon2.JPG';
 import cocoon3 from '../Images/cocoon3.JPG';
@@ -11,6 +11,9 @@ import scandinave2 from '../Images/scandinave2.JPG';
 import scandinave3 from '../Images/scandinave3.JPG';
 import scandinave4 from '../Images/scandinave4.JPG' ;
 import scandinave5 from '../Images/scandinave5.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 
 
 export default function PrivateRoom(){
@@ -31,14 +34,12 @@ export default function PrivateRoom(){
     return(
         <div className="PrivateContainerRoom">
                 <div className='room'>
-                <div className="RoomHeader">Chambres<b></b></div>
                 <div className="cocoon-box">
-                    <h1>COCOON</h1>
-                    <div className="cocoon-diapo"><Diaporama picturesList ={picturesList1}/></div>
+                    <h2>Chambre 1 : Univers Fragole Au Rez-de-chaussée Accessible Pmr</h2>
                     <div className="cocoon-info">
                         <div className="cocoon1">
-                            <h2>Chambre 1 : Univers Fragole Au Rez-de-chaussée Accessible Pmr</h2>
                             <p>Entrée indépendante. Au rez-de-chaussée, accessible aux personnes à mobilité réduite. 1 lit 160 X 200, wc indépendants, salle d'eau avec vasque et douche à l'italienne (180 x 150). Possibilité d'un lit supplémentaire de 90 x 200. Wifi, TV écran plat.</p>
+                            <div className="cocoon-diapo"><Diaporama picturesList ={picturesList1}/></div>
                         </div>
                         <div className="cocoon2">
                             <li>1x Lit simple lits 90cm</li>
@@ -48,36 +49,106 @@ export default function PrivateRoom(){
                             <li>Télévision privée</li>
                             <li>Cuisine commune</li>
                         </div>
-                        <div className="cocoon3" onClick={()=>ShowPrice1()}>
-                            <b>A partir de 95€</b><br></br>
-                            <p>Par nuit base 2 personnes</p>
-                            <p>Petits déjeuners inclus</p>
-                            <p>Voir le détail des prix</p>
+                        <div className="cocoon3">
+                            <div className="cocoon31">
+                                <b>A partir de 95€</b>
+                                <p>Par nuit base 2 personnes</p>
+                                <p>Petits déjeuners inclus</p>
+                            </div>
+                            <div className="cocoon32" onClick={()=>ShowPrice1()}>
+                                <p>Voir le détail des prix</p>
+                            </div>
                         </div>
                     </div>
-                    {prix1?<div>hola qué tal?</div>:false}
+                    {prix1?
+                        <div className="prix" data-aos="fade-right" data-aos-duration="1000">
+                            <div className="prix1">
+                                <p>1 pers : 1€</p>
+                                <p>2 pers : 2€</p>
+                            </div>
+                            <div className="prix2">
+                                <p>3 pers : 3€</p>
+                                <p>4 pers : 4€</p>
+                            </div>
+                        </div>:false}
                 </div>
-                <div className="fragole-box">
-                    <h1>FRAGOLE</h1>
-                    <div className='fragole-diapo'><Diaporama picturesList ={picturesList1}/></div>
-                    <div className="fragole-info">
-                        <div className="fragole1">rdb</div>
-                        <div className="fragole2">vrdvr</div>
-                        <div className="fragole3" onClick={()=>ShowPrice2()}>cliquez ici</div>
+                <div className="cocoon-box">
+                    <h2>Chambre 2 : Univers Scandinave à L'étage</h2>
+                    <div className="cocoon-info">
+                        <div className="cocoon1">
+                            <p>Chambre Scandinave à l'étage : 2 lits 90/200 ou 1 lit 180/200, wc indépendant, salle d'eau (vasque, douche plate 180 X 80), possibilité 2 lits 90x200cm supplémentaires. Wifi. TV écran plat.</p>
+                            <div className="cocoon-diapo"><Diaporama picturesList ={picturesList1}/></div>
+                        </div>
+                        <div className="cocoon2">
+                            <li>2x Lit simple lits 90cm</li>
+                            <li>1x Lit double lits 160cm ou supérieur</li>
+                            <li>2x Lit bébé</li>
+                            <li>WC privé</li>
+                            <li>Douche privée</li>
+                            <li>Télévision privée</li>
+                            <li>Douche commune</li>
+                        </div>
+                        <div className="cocoon3">
+                            <div className="cocoon31">
+                                <b>A partir de 95€</b>
+                                <p>Par nuit base 2 personnes</p>
+                                <p>Petits déjeuners inclus</p>
+                            </div>
+                            <div className="cocoon32" onClick={()=>ShowPrice2()}>
+                                <p>Voir le détail des prix</p>
+                            </div>
+                        </div>
                     </div>
-                    {prix2?<div>como te llamas?</div>:false}
+                    {prix2?
+                        <div className="prix" data-aos="fade-right" data-aos-duration="1000">
+                            <div className="prix1">
+                                <p>1 pers : 1€</p>
+                                <p>2 pers : 2€</p>
+                            </div>
+                            <div className="prix2">
+                                <p>3 pers : 3€</p>
+                                <p>4 pers : 4€</p>
+                            </div>
+                        </div>:false}
                 </div>
                 <div className="scandinave-box">
-                    <h1>SCANDINAVE</h1>
-                    <div className='scandinave-diapo'><Diaporama picturesList ={picturesList2}/></div>
-                    <div className="scandinave-info">
-                        <div className="scandinave1">rdb</div>
-                        <div className="scandinave2">vrdvr</div>
-                        <div className="scandinave3" onClick={()=>ShowPrice3()}>cliquez ici</div>
+                    <h2>Chambre 3 : Univers Cocoon à L'étage</h2>
+                    <div className="cocoon-info">
+                        <div className="cocoon1">
+                            <p>Chambre Cocoon à l'étage : 2 lits 90 x 200 (ou 1 lit 180), wc indépendant, salle de bains (vasque, baignoire) possibilité 3 lits 90x200cm supplémentaire. TV écran plat par chambre. Wifi.</p>
+                            <div className="cocoon-diapo"><Diaporama picturesList ={picturesList2}/></div>
+                        </div>
+                        <div className="cocoon2">
+                            <li>3x Lit simple lits 90cm</li>
+                            <li>1x Lit double lits 160cm ou supérieur</li>
+                            <li>2x Lit bébé</li>
+                            <li>Salle de bain privée</li>
+                            <li>WC privé</li>
+                            <li>Télévision privée</li>
+                        </div>
+                        <div className="cocoon3">
+                            <div className="cocoon31">
+                                <b>A partir de 100€</b>
+                                <p>Par nuit base 2 personnes</p>
+                                <p>Petits déjeuners inclus</p>
+                            </div>
+                            <div className="cocoon32" onClick={()=>ShowPrice3()}>
+                                <p>Voir le détail des prix</p>
+                            </div>
+                        </div>
                     </div>
-                    {prix3?<div>cuandos anos tienes?</div>:false}
+                    {prix3?
+                        <div className="prix" data-aos="fade-right" data-aos-duration="1000">
+                            <div className="prix1">
+                                <p>1 pers : 1€</p>
+                                <p>2 pers : 2€</p>
+                            </div>
+                            <div className="prix2">
+                                <p>3 pers : 3€</p>
+                                <p>4 pers : 4€</p>
+                            </div>
+                        </div>:false}
                 </div>
-                <iframe  height='750' width='100%' src='https://widget.itea.fr/widget.php?key=amkkhds7&numGite=2697&widget-itea=prix' ></iframe> 
             </div>
         </div>
     );
