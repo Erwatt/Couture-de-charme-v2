77067dc541c68277e91e36866cd506b8412f4cc9
MINUCCITECH <50174694+Monpuis@users.noreply.github.com>
2021-10-01 10:37:25 +0200

    Update MentionsLegales.js

--- la-couture-du-charme/client/src/Components/MentionsLegales.js
+++ la-couture-du-charme/client/src/Components/MentionsLegales.js
@@ -26,6 +26,10 @@ function MentionsLegales() {
                 <div>
                     <p>Le présent site est édité par Nicolas Defoort, Mathias Mege et Erwan Wattelier</p>
                     <p>Responsable de publication : Valérie Lictevout</p>
+        <br/>
+                    <p>S.A.R.L. SAMEO inscrite au R.C.S. de Douai sous le n°518.905.534</p>
+                    <p>1400 RUE DE BEAUMETZ à SAMEON (59310)</p>
+    <br/>
                     <p>Hébergeur : 1 & 1 Ionos SARL, 7 Place de la gare, 57200 Sarreguemines</p><br></br>
                 </div>
                 <h2>OBJET DU SITE INTERNET</h2>
@@ -34,7 +38,7 @@ function MentionsLegales() {
                 <h2>DROITS D'AUTEURS</h2>
                 <p>Que ce soit la présentation visuelle, le logo, le contenu écrit, le contenu audiovisuelle, les photographies, les fonctionnalités du site internet... L'ensemble de ces éléments sont protégés par le droit d'auteur et ne peuvent être reproduits sans l'accord préalable du cabinet.</p>
                 <br />
-                <h2>DONNEES A CARACTERE PERSONNELLE</h2>
+                <h2>DONNEES A CARACTERE PERSONNEL</h2>
                 <p>Le présent site internet utilise des cookies dans le but de connaître les chiffres de fréquentation de ses différentes pages. L'objectif est de vous proposer du contenu de meilleur qualité, qui réponde parfaitement à vos attentes. Vos données personnelles sont collectées lors d'une prise de contact avec notamment, notamment via le formulaire, lorsque vous prenez rendez-vous ou encore lorsque vous créez votre espace personnel en ligne. Vous est demandé, uniquement les informations nécessaires pour pouvoir vous recontacter ou répondre à votre problématique. Nous n'utiliserons pas ces informations dans un but de prospection, ni ne communiquerons ces données à un tiers.</p>
                 <p>Conformément au RGPD (règlement général sur la protection des données), vous disposez d’un droit d’information, d’accès, de rectification, d’effacement, de limitation, de portabilité et d’opposition sur vos données personnelles. Pour exercer ce droit, contactez-nous.</p>
             </div>
