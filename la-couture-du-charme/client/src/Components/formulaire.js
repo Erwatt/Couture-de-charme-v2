adc5c2f0558cf5eaab0570104ec20a20a458014c
MINUCCITECH <50174694+Monpuis@users.noreply.github.com>
2021-10-01 10:27:44 +0200

    Update formulaire.js

--- la-couture-du-charme/client/src/Components/formulaire.js
+++ la-couture-du-charme/client/src/Components/formulaire.js
@@ -65,10 +65,10 @@ export default function NewFormulaire() {
         //http://localhost:8000/post
         //https://domaine.com/post
         axios.post('http://localhost:3001/api/SendMessage', {
-            nom: name,
-            email: mail,
-            telephone: phone,
-            message: message
+            name,
+            mail,
+            phone,
+            message
 
         }
         )
