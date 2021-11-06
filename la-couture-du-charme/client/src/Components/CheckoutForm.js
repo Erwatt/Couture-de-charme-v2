218a6cddde0085adeefb2ef38458e81ee060bc8a
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-11-06 12:34:56 +0100

    modif checkout form

--- la-couture-du-charme/client/src/Components/CheckoutForm.js
+++ la-couture-du-charme/client/src/Components/CheckoutForm.js
@@ -14,7 +14,7 @@ export default function CheckoutForm({element,prix, ligne1, event, from, to, mai
   const [error, setError] = useState(null);
   const [processing, setProcessing] = useState('');
   const [disabled, setDisabled] = useState(true);
-  const [clientSecret, setClientSecret] = useState('');
+  // const [clientSecret, setClientSecret] = useState('');
 
   // useEffect(() => {
   //   // Create PaymentIntent as soon as the page loads
@@ -62,12 +62,12 @@ export default function CheckoutForm({element,prix, ligne1, event, from, to, mai
   //   }
   // };
 
-  // const handleChange = async (event) => {
-  //   // Listen for changes in the CardElement
-  //   // and display any errors as the customer types their card details
-  //   setDisabled(event.empty);
-  //   setError(event.error ? event.error.message : "");
-  // };
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   // const handleSubmit = async ev => {
   //   ev.preventDefault();
@@ -94,7 +94,9 @@ export default function CheckoutForm({element,prix, ligne1, event, from, to, mai
     const stripe = useStripe()
     const elements = useElements()
 
-    const pay = async () =>{
+    const pay = async (ev) =>{
+      ev.preventDefault();
+     setProcessing(true);
       try{
         const response = await fetch('http://localhost:3001/pay', {
           method: 'POST',
@@ -111,8 +113,11 @@ export default function CheckoutForm({element,prix, ligne1, event, from, to, mai
         })
 
         const {paymentIntent} = confirmPayment
-        if(paymentIntent.status === 'succeeded') alert('Paiement effectué avec succès !')
-        else alert('Le paiement a échoué !')
+        if(paymentIntent.status === 'succeeded'){
+          alert('Paiement effectué avec succès !')
+          setSucceeded(true);
+        }
+        else setSucceeded(false);
 
       } catch(err){
         console.error(err)
@@ -132,12 +137,33 @@ export default function CheckoutForm({element,prix, ligne1, event, from, to, mai
   return (
 
     // NOUVEAU CODE DAVID
-    <div className="checkout">
-
-      <CardElement />
-      <button onClick={pay}>Commander</button>
-
-    </div>
+    
+      <form onSubmit={pay}>
+      {!succeeded ? (
+        <div>
+      <div className="checkout">
+      <CardElement onChange={handleChange}/>
+        {/* <button onClick={pay}>Commander</button>  */}
+      </div>
+      <button disabled={processing || disabled || succeeded} id="submit">
+           <span id="button-text">
+           {processing ? (
+             <div className="spinner" id="spinner"></div>) : ("Commander")}
+         </span>
+       </button>
+       {/* Show any error that happens when processing the payment */}
+       {error && (
+         <div className="card-error" role="alert">
+           {error}
+         </div>
+       )}
+         </div>):
+         <p className="successMessage">
+         Votre réservation a bien été prise en compte. Merci !
+       </p>
+       }
+       </form>
+       
 
 // ANCIEN CODE
     // <div className="PaymentContainer">
