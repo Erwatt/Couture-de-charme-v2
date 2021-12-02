7887afb2258e2bb8e0cd99876f811d84ce356f46
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-12-02 11:25:12 +0100

    Modif build

--- la-couture-du-charme/client/src/services.js
+++ la-couture-du-charme/client/src/services.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/';
+// const API_URL = 'http://localhost:3001/api/';
+const API_URL = 'https://lacoutureducharme.herokuapp.com/api/'
 
 class services {
     sendMessage(name, phone, mail, spam, message){
--- la-couture-du-charme/server.js
+++ la-couture-du-charme/server.js
@@ -60,7 +60,7 @@ server.on('listening', () => {
   console.log('Listening on ' + bind);
 });
 
-// app.use(express.static(path.resolve(__dirname,'./client/build')));
+app.use(express.static(path.resolve(__dirname,'./client/build')));
 
 
 
@@ -85,9 +85,9 @@ app.use('/api', route);
 // });
 
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+})
 
 
 // NOUVEAU CODE STRIPE DAVID
